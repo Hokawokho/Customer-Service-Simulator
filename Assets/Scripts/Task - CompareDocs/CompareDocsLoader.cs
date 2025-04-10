f1d3f7289450bfb4572bc4cecff1948d65142eb6
Dabare02 <104628493+Dabare02@users.noreply.github.com>
2025-04-10 21:11:39 +0200

    Script para añadir lineas (bugeado)

---
+++ Assets/Scripts/Task - CompareDocs/CompareDocsLoader.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+// WIP (quizás rehacer con un approach distinto)
+public class CompareDocsLoader : MonoBehaviour
+{
+
+    private string inputLinesPath;
+    private string regularLinesPath;
+
+    public int maxInputLines = 3;
+    public int maxRegularLines = 7;
+    [SerializeField] private GameObject inputLinePrefab;
+    [SerializeField] private GameObject regularLinePrefab;
+    [SerializeField] private GameObject docContents;
+
+    void Start()
+    {
+        // TODO: Cambiar a Application.persistentDataPath + "/Data/CompareDoc"
+        // (y lidiar con los problemas que eso causa...)
+        // Porque persistentDataPath es para los datos que persistirán entre ejecuciones.
+        inputLinesPath = Application.dataPath + "/Data/CompareDoc/InputLine";
+        regularLinesPath = Application.dataPath + "/Data/CompareDoc/RegularLine";
+
+        LoadLines();
+    }
+
+    private void LoadLines() {
+        List<string> inputLineFiles = Directory.GetFiles(inputLinesPath, "*.json").ToList();
+        List<string> regularLineFiles = Directory.GetFiles(regularLinesPath, "*json").ToList();
+
+        if (inputLineFiles.Count == 0 || regularLineFiles.Count == 0) {
+            Debug.LogError("No se encontraron archivos json en " + inputLineFiles
+                + "o " + regularLinesPath);
+            return;
+        }
+
+        for (int i = 0; i < maxInputLines; i++) {
+            int r = Random.Range(0, inputLineFiles.Count);
+            string selectedFile = inputLineFiles[r];
+            string json = File.ReadAllText(selectedFile);
+            LineData inputLine = JsonUtility.FromJson<LineData>(json);
+
+            ApplyLine(inputLine, true);
+        }
+        for (int i = 0; i < maxRegularLines; i++) {
+            int r = Random.Range(0, regularLineFiles.Count);
+            string selectedFile = regularLineFiles[r];
+            string json = File.ReadAllText(selectedFile);
+            LineData regularLine = JsonUtility.FromJson<LineData>(json);
+
+            ApplyLine(regularLine, false);
+        }
+    }
+
+    private void ApplyLine(LineData line, bool isInputLine) {
+        if (isInputLine) {
+            GameObject inputLine = Instantiate(inputLinePrefab, docContents.transform);
+            TMP_Text text = inputLine.transform.GetChild(0).GetComponent<TMP_Text>();
+            text.text = line.text;
+
+            //TODO
+        } else {
+            GameObject regularLine = Instantiate(regularLinePrefab, docContents.transform);
+            TMP_Text text = regularLine.GetComponent<TMP_Text>();
+            text.text = line.text;
+        }
+    }
+
+}
+
+[Serializable]
+public class LineData {
+    public string text;
+    public string desiredRes;
+}
