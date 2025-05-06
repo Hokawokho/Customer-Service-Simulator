b91c163ac214323e505b42baef12d0f33848a87f
Dabare02 <dabare02@gmail.com>
2025-05-07 01:41:13 +0200

    Funcionalidad de pausa en escena principal.

---
+++ Assets/Scripts/OfficeManager/PlayPauseManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayPauseManager : MonoBehaviour
+{
+    [Tooltip("Añadir aqui GameObjects que sigan siendo interactuables con el menú"
+        +  "de pausa abierto. No tocar \"State Before Pause\".")]
+    [SerializeField] private List<UnpausableObject> extraObjectsToPause;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause() {
+        // Pausar/reanudar juego.
+        GeneralManager.Instance.pause = !GeneralManager.Instance.pause;
+        bool isGamePaused = GeneralManager.Instance.pause;
+
+        // Comprobar si hay items extra que desactivar
+        if (extraObjectsToPause != null) {
+            foreach (UnpausableObject unObj in extraObjectsToPause) {
+                GameObject obj = unObj.objectToPause;
+                if (isGamePaused) unObj.activeBeforePause = obj.activeSelf;
+                    
+                // Si el objeto es fatherScript, usar su función específica.
+                fatherScript fScr = obj.GetComponent<fatherScript>();
+                if (fScr != null) {
+                    if (isGamePaused || !unObj.activeBeforePause) fScr.DisableAll();
+                    else if (unObj.activeBeforePause) fScr.EnableAll();
+                } else {
+                    if (isGamePaused) obj.SetActive(false);
+                    else obj.SetActive(unObj.activeBeforePause);
+                }
+            }
+        }
+    }
+
+}
+
+[Serializable]
+public class UnpausableObject {
+    public GameObject objectToPause;
+    public bool activeBeforePause;
+}
